ff0ab13abc270f186260147bd6026cefed6817d8
cangyuan <1339918746@qq.com>
2017-01-12 13:32:23 +0800

    test

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('myapp', ['ui.router']);
+app.config(function($stateProvider, $urlRouterProvider,$httpProvider) {
+    // 解决router缓存问题
+    if (!$httpProvider.defaults.headers.get) {
+          $httpProvider.defaults.headers.get = {};
+        }
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    // 解决router缓存问题 end
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state({
+            name: "/",
+            url: "/",
+            templateUrl: 'tpls/home.html',
+            controller: 'homeConstoller'
+        })	
+})
